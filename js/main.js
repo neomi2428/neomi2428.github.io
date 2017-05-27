86493ad03cbdba9add1ca78d73c206d0fbbec539
Junhee Park <neomi2428@gmail.com>
2017-05-27 05:18:14 -0700

    Feature/single page app (#1)
    * attache an event listener to menus
    
    * made MenuHandler object
    
    * updated styles at Sat May 27 03:58:52 PDT 2017
    
    * updated styles at Sat May 27 03:59:45 PDT 2017
    
    * updated sh
    
    * refactor menu selector
    
    * deleted about page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 /*jslint browser:true*/
 /*global window*/
+
+// This method is for template/main.html
+// It dynamically loads DOMs in the above file.
 const loadTemplates = (a) => {
     var load = a.concat('load event detected!');
     console.log(load);
@@ -20,9 +23,63 @@ const loadFont = () => {
     document.head.insertAdjacentElement('afterbegin', link);
 };
 
-const initialize = () => {
-    loadTemplates('a ');
-    loadFont();
+class MenuHandler {
+    // menuArray should have id and url properties
+    // add validation steps to make sure this
+    constructor (menuArray) {
+        this.menuArray = menuArray
+        this.homeMenu = menuArray[0]
+        this.currentMenuClassName = "current-menu"
+    }
+    initialize() {
+        for (var menu of this.menuArray) {
+            (function(menuModel) {
+
+                var menuElement = document.getElementById(menuModel.id);
+                if (menuModel.id === this.homeMenu.id) {
+                    menuElement.classList.add(this.currentMenuClassName);
+                }
+
+                menuElement.addEventListener("click", (function() {
+                    this.onClick(menuModel, menuElement);
+                }).bind(this), false);
+
+            }).bind(this)(menu);
+        }
+    }
+    onClick(menuModel, menuElement) {
+        history.pushState(menuModel, menuModel.id, menuModel.url);
+        this.selectMenu(menuElement);
+        document.getElementsByTagName('content')[0].innerHTML = menuModel.content;
+    }
+    selectMenu(menuElement) {
+        for (var menu of this.menuArray) {
+            document.getElementById(menu.id).classList.remove(this.currentMenuClassName);
+        }
+        menuElement.classList.add(this.currentMenuClassName);
+    }
+}
+
+const setContent = (content) => {
+    ;
 };
 
+const initialize = () => {
+    loadFont();
+    menuHandler.initialize();
+    history.replaceState(menuHandler.homeMenu, menuHandler.homeMenu.id, "");
+};
+
+const menuHandler = new MenuHandler([
+    { "id": "home-menu", "url": "home", "content": "Hello, World!" },
+    { "id": "about-menu", "url": "about", "content": "About" }
+]);
+
 window.onload = initialize();
+window.onpopstate = function(event) {
+    if (event.state) {
+        var menuModel = event.state;
+        menuHandler.selectMenu(document.getElementById(menuModel.id));
+        document.getElementsByTagName('content')[0].innerHTML = menuModel.content;
+    }
+}
