f980903673fa18c99d79f8e195197f9c963437f5
Junhee Park <neomi2428@gmail.com>
2017-06-27 23:30:25 -0700

    first update for header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
+
   <div id="app">
-    <img src="./assets/logo.png">
+    <header>
+      <h1>Enabler, Equalizer</h1>
+    </header>
+    <nav>
+      <ul>
+      </ul>
+    </nav>
+    <!-- <img src="./assets/logo.png"> -->
     <router-view></router-view>
   </div>
+
 </template>
 
 <script>
+
 export default {
   name: 'app'
 }
+
 </script>
 
 <style>
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font: 100% "Open Sans", Helvetica, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
